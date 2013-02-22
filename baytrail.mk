39e1bf4819a703e64644a26cd70d5698f2959b3d
Brian Wood <brian.j.wood@intel.com>
2013-02-22 15:24:09 -0800

    Baytrail-Integration: Enable adbd service through init
    BZ: 88815
    
    This commit enables the adbd service through the normal
    method of using the init process like is used for our
    other Intel platforms.
    
    Change-Id: I75f268c10f28d5806255c449dd2a8196435a346f
    Signed-off-by: Brian Wood <brian.j.wood@intel.com>
    Reviewed-on: http://android.intel.com:8080/93406
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Tested-by: Ng, Cheon-woei <cheon-woei.ng@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- baytrail.mk
+++ baytrail.mk
@@ -3,3 +3,8 @@ TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_BOARD_PLATFORM)
 
 # Include common environnement
 include vendor/intel/common/common.mk
+
+# USB port turn around and initialization
+USB_PATH := vendor/intel/baytrail
+PRODUCT_COPY_FILES += \
+        $(USB_PATH)/init.byt.usb.rc:root/init.platform.usb.rc
