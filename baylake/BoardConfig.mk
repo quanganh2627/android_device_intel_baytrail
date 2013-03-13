5ad5682e532834bf039bc746056069392da71be1
Guilhem IMBERTON <guilhem.imberton@intel.com>
2013-03-13 15:23:45 +0100

    Platform-conf: Allow overriding REF_*_NAME
    BZ: 85272
    
    Customer can override REF_*_NAME while the variables are automatically
    populated for internal builds.
    
    Change-Id: I159b38ca5999895fbae8adacf8f2aad1c03f1656
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>
    Reviewed-on: http://android.intel.com:8080/96414
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Tested-by: Fert, Laurent <laurent.fert@intel.com>
    Reviewed-by: Wood, Brian J <brian.j.wood@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- baylake/BoardConfig.mk
+++ baylake/BoardConfig.mk
@@ -1,7 +1,7 @@
 # Name of the reference design
 # Should be changed with the original values when starting customization
-REF_DEVICE_NAME := $(TARGET_DEVICE)
-REF_PRODUCT_NAME := $(TARGET_PRODUCT)
+REF_DEVICE_NAME ?= $(TARGET_DEVICE)
+REF_PRODUCT_NAME ?= $(TARGET_PRODUCT)
 
 DEVICE_PATH := vendor/intel/baytrail/baylake
 
