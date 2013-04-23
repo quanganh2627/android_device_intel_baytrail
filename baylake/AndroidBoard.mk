1c0710f6952ea22a7ef0d431252156c7ed231484
Pierre Tardy <pierre.tardy@intel.com>
2013-04-23 16:09:47 +0200

    iafw: integrate iafw build into baylake
    BZ: 102925
    
    Change-Id: I373d29edfe3d249b047dea5a1252738dab33d6ae
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>
    Reviewed-on: http://android.intel.com:8080/104162
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>

--- baylake/AndroidBoard.mk
+++ baylake/AndroidBoard.mk
@@ -68,7 +68,7 @@ $(PRODUCT_OUT)/startup.nsh: $(PRODUCT_OUT)/kernel $(TARGET_DEVICE_DIR)/BoardConf
 	echo "fs0:\kernel.efi $(BOARD_KERNEL_CMDLINE) initrd=ramdisk.img" >> "$(PRODUCT_OUT)/startup.nsh"
 
 ### TEMPORARY: override flashfiles defined in common until baytrail supports them.
-flashfiles: $(PRODUCT_OUT)/partition.tbl
+flashfiles: $(PRODUCT_OUT)/partition.tbl baylake_ifwi
 	@$(eval FLASHFILE_PATH := $(PUBLISH_PATH)/$(TARGET_PUBLISH_PATH)/flash_files/build-$(PUBLISH_TARGET_BUILD_VARIANT))
 	@$(eval FLASHFILE_NAME := $(GENERIC_TARGET_NAME)-$(PUBLISH_TARGET_BUILD_VARIANT)-fastboot-$(FILE_NAME_TAG).zip)
 	@echo "Generating $(FLASHFILE_PATH)/$(FLASHFILE_NAME)"
@@ -83,7 +83,8 @@ flashfiles: $(PRODUCT_OUT)/partition.tbl
 	@cp $(TARGET_DEVICE_DIR)/flash.xml $(FLASHFILE_PATH)/
 	@cp $(TARGET_DEVICE_DIR)/flash_capsule.xml $(FLASHFILE_PATH)/
 	@cp $(OUT)/partition.tbl $(FLASHFILE_PATH)/
-	@cp $(IFWI_PREBUILT_PATHS)/capsule.bin $(PRODUCT_OUT)/byt_psi_encapsulated_ifwi.bin
+	@cp $(TARGET_OUT_IFWIS)/ifwi_capsule.bin $(PRODUCT_OUT)/byt_psi_encapsulated_ifwi.bin
+	@cp $(TARGET_OUT_IFWIS)/ifwi.bin $(FLASHFILE_PATH)/dediprog.rom
 	@cp $(PRODUCT_OUT)/byt_psi_encapsulated_ifwi.bin $(FLASHFILE_PATH)/
 	@zip -j $(FLASHFILE_PATH)/$(FLASHFILE_NAME) $(FLASHFILE_PATH)/*
 	@find $(FLASHFILE_PATH) -name '*.zip' -prune -o -type f -exec rm {} \;
