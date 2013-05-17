0df20f0189d28e70abbcf81ceacbdb0959efbc01
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-05-17 10:40:29 +0200

    Remove Deprecated checks
    BZ: 108694
    
    Remove check to TARGET_KERNEL_SOURCE_IS_PRESENT
    (it's now handled in kernel/AndroidKernel.mk automatically)
    
    Change-Id: Id1bc5c79a3e8065fa51c513b9880883d7d0c7f3a
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: http://android.intel.com:8080/108122
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,13 +3,11 @@
 
 include device/intel/common/AndroidBoard.mk
 
-ifneq ($(TARGET_KERNEL_SOURCE_IS_PRESENT),false)
 # Add socwatchdk driver
 -include $(TOP)/linux/modules/debug_tools/socwatchdk/src/AndroidSOCWatchDK.mk
 
 # Add VISA driver
 -include $(TOP)/vendor/intel/tools/PRIVATE/debug_internal_tools/visadk/driver/src/AndroidVISA.mk
-endif #TARGET_KERNEL_SOURCE_IS_PRESENT
 
 # wifi
 ifeq ($(strip $(BOARD_HAVE_WIFI)),true)
