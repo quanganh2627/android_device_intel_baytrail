888adc74ca2ccb6c28e0e067b6f73f216f5ab182
Laurent FERT <laurent.fert@intel.com>
2013-05-30 18:52:42 +0200

    Enabled the drm debug messages
    BZ:107529
    
    Change-Id: I6096f699fe49a6a4f213c71277f5f1732220cfec
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: http://android.intel.com:8080/110847
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Imberton, Guilhem <guilhem.imberton@intel.com>

--- byt_t_ffrd10/BoardConfig.mk
+++ byt_t_ffrd10/BoardConfig.mk
@@ -60,7 +60,7 @@ JPEGDEC_USES_GEN := true
 
 ifeq ($(BOARD_KERNEL_CMDLINE),)
 ifeq ($(TARGET_BUILD_VARIANT),eng)
-BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=8 kmemleak=off ptrace.ptrace_can_access=1 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=8 drm.debug=0xe kmemleak=off ptrace.ptrace_can_access=1 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
 else ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=4 kmemleak=off ptrace.ptrace_can_access=1 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
 else
