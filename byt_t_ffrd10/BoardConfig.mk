3ecd83a4636cf3d243d6bf5d23de24170267141e
Sumit Jain <sumit.jain@intel.com>
2013-06-04 02:43:52 +0530

    Disabling PSR for FFRD10 through command line parameter
    BZ: 111955
    
    The command line parameter 'i915.psr_support' can be used to enable/disable PSR
    as a feature on the platform.
    0 = PSR not supported
    1 = PSR supported (default)
    
    Change-Id: I36ed374227bedea382b9c15d4314ae478d745fae
    Signed-off-by: Sumit Jain <sumit.jain@intel.com>

--- byt_t_ffrd10/BoardConfig.mk
+++ byt_t_ffrd10/BoardConfig.mk
@@ -72,7 +72,7 @@ endif
 BOARD_GPU_DRIVERS := i965
 USE_OPENGL_RENDERER := true
 BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 \
-                        acpi_backlight=vendor
+                        acpi_backlight=vendor i915.psr_support=0
 
 BOARD_USES_LIBPSS := false
 
