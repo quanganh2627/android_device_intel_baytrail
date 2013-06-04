0c99f1047756c8af730f2ab2d74b0e19e6791bbc
Pallavi <pallavi.g@intel.com>
2013-06-04 12:31:03 +0530

    Disabled the DRM debug Messages
    BZ:113321
    
    Disabled the DRM debug Messages which leads to
    lot of instability/delay in the system
    
    Change-Id: I4d05f81f1c8d642e76c9aa903283ff682675e726
    Signed-off-by: Pallavi <pallavi.g@intel.com>
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>
    Reviewed-on: http://android.intel.com:8080/111493
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Tested-by: Sun, Hang L <hang.l.sun@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- baylake/BoardConfig.mk
+++ baylake/BoardConfig.mk
@@ -72,7 +72,7 @@ JPEGDEC_USES_GEN := true
 
 ifeq ($(BOARD_KERNEL_CMDLINE),)
 ifeq ($(TARGET_BUILD_VARIANT),eng)
-BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=8 drm.debug=0xe kmemleak=off ptrace.ptrace_can_access=1 emmc_ipanic.ipanic_part_number=3 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=8 drm.debug=0x0 kmemleak=off ptrace.ptrace_can_access=1 emmc_ipanic.ipanic_part_number=3 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
 else ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 BOARD_KERNEL_CMDLINE := console=ttyS0,115200 console=logk0 earlyprintk=nologger loglevel=4 kmemleak=off ptrace.ptrace_can_access=1 emmc_ipanic.ipanic_part_number=3 androidboot.bootmedia=$(BOARD_BOOTMEDIA) androidboot.hardware=$(TARGET_DEVICE) $(cmdline_extra) nmi_watchdog=panic softlockup_panic=1
 else
