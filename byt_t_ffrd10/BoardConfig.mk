7856e05e8ecb48a6f1160a2c134ac53777fb49d0
Mazhar <mazharx.sameullah@intel.com>
2013-06-17 16:22:56 +0530

    [BYT] Enable WIDI/ HDMI HAL
    BZ: 116585
    
    The product/ package config updated to take HDMI
    and WIDI audio related changes
    
    Change-Id: Ib6db9f3695ff650a9e02d5e194bac50caa55ead8
    Signed-off-by: Mazhar <mazharx.sameullah@intel.com>
    Reviewed-on: http://android.intel.com:8080/113800
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Anand, Jerome <jerome.anand@intel.com>
    Reviewed-by: Krishna, Samaga <samaga.krishna@intel.com>
    Reviewed-by: Sikkandar D, Madar <madar.sikkandar.d@intel.com>
    Tested-by: Gupta, ArvindX K <arvindx.k.gupta@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- byt_t_ffrd10/BoardConfig.mk
+++ byt_t_ffrd10/BoardConfig.mk
@@ -32,6 +32,7 @@ INTEL_WIDI_BAYTRAIL := true
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_TINY_ALSA_AUDIO := true
 BOARD_USES_AUDIO_HAL_CONFIGURABLE := true
 BOARD_USE_VIBRATOR_ALSA := false
 BUILD_WITH_ALSA_UTILS := true
@@ -89,6 +90,9 @@ USE_HW_VP8 := true
 BOARD_USE_LIBVA_INTEL_DRIVER := true
 BOARD_USE_LIBVA := true
 BOARD_USE_LIBMIX := true
+
+#Support background music playback for Widi Multitasking
+ENABLE_BACKGROUND_MUSIC := true
 
 # Settings for the Media SDK library and plug-ins:
 # - USE_MEDIASDK: use Media SDK support or not
