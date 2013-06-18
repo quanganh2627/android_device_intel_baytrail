2541a8dacad95dfcd239e41ef35f130cecebf514
jli119X <jianpingx.li@intel.com>
2013-06-18 14:58:44 +0800

    Disable debugable on user build
    BZ: 110322
    
    We need to disable debugable on user build to pass CTS test.
    Use init.nodebug.rc instead of init.debug.rc on user build
    
    Change-Id: I99b021271f8cbaeda8b9345b3987bed36398692f
    Signed-off-by: jli119X <jianpingx.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/114008
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Reviewed-by: Kundella, SreedharX <sreedharx.kundella@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- baytrail.mk
+++ baytrail.mk
@@ -7,10 +7,18 @@ include device/intel/common/common.mk
 # USB port turn around and initialization
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/init.byt.usb.rc:root/init.platform.usb.rc \
-    $(PLATFORM_PATH)/init.debug.rc:root/init.debug.rc \
     $(PLATFORM_PATH)/props.baytrail.rc:root/props.platform.rc \
     $(PLATFORM_PATH)/maxtouch.fw:system/etc/firmware/maxtouch.fw \
     $(PLATFORM_PATH)/mxt1664S-touchscreen.idc:system/usr/idc/mxt1664S-touchscreen.idc
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/init.nodebug.rc:root/init.debug.rc
+else
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/init.debug.rc:root/init.debug.rc
+endif
+
 
 # Kernel Watchdog
 PRODUCT_COPY_FILES += \
