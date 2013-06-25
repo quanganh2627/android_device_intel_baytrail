b584f7274b5e98107c67bc4f33a2f4e6b5822c05
Sergio Aguirre <sergio.a.aguirre.rodriguez@intel.com>
2013-06-25 09:14:54 -0700

    OSIP installer: Update create_installer.sh to use new blankphone zip
    BZ: 118936
    
    Adapt to look for blankphone and fastboot zip files, which is currently
    generated in latest builds in mainline as of June 25 2013.
    
    Also, droidboot will be taken from the zip file, instead of requiring
    someone to extract it by themselves.
    
    Change-Id: Ibac32c11586bb9453bad83785492af7d1f8aaa19
    Signed-off-by: Sergio Aguirre <sergio.a.aguirre.rodriguez@intel.com>
    Signed-off-by: Frederic Despalles <frederic.fespalles@intel.com>
    Reviewed-on: http://android.intel.com:8080/127984
    Reviewed-by: Fradin, Benoit <benoit.fradin@intel.com>
    Reviewed-by: Yang, Fei <fei.yang@intel.com>
    Reviewed-by: Fert, Laurent <laurent.fert@intel.com>
    Tested-by: Lumbierres, PhilippeX <philippex.lumbierres@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- create_installer.sh
+++ create_installer.sh
@@ -6,10 +6,10 @@
 
 if [ $# -eq 3 ]; then
     disk=$1
-    droidboot_image=$2
-    build_file=$3
+    blankphone_zip=$2
+    fastboot_zip=$3
 else
-    echo "Usage: $0 block_device droidboot.img flashfile.zip"
+    echo "Usage: $0 block_device blankphone.zip fastboot.zip"
     exit
 fi
 
@@ -18,15 +18,12 @@ if [ ! -b /dev/${disk} ]; then
     exit
 fi
 
-disk_mounted=`mount | grep -c $disk`
+#Unmount all the listed partitions under "disk" device name
+mount | grep ${disk} | cut -d' ' -f1 | xargs umount
 
-if [ $disk_mounted -ge 1 ]; then
-    echo "$disk seems to be mounted... Aborting"
-    exit
-fi
+unzip ${blankphone_zip} droidboot.img -d /tmp
 
-
-dd if=${droidboot_image} of=/dev/${disk} bs=1M
+dd if=/tmp/droidboot.img of=/dev/${disk} bs=1M
 
 echo "unit: sectors" > /tmp/droidboot_installer_partition
 echo "/dev/${disk}1 : start=   524322, size=  4194304, Id=83" >> /tmp/droidboot_installer_partition
@@ -37,7 +34,10 @@ echo "/dev/${disk}4 : start=        0, size=        0, Id= 0" >> /tmp/droidboot_
 sfdisk /dev/${disk} < /tmp/droidboot_installer_partition
 mkfs.vfat /dev/${disk}1
 mount /dev/${disk}1 /mnt
-echo "Unzip $build_file to /mnt"
-unzip $build_file -d /mnt
+echo "Unzip contents to USB installer"
+mv /tmp/droidboot.img /mnt/
+unzip ${blankphone_zip} boot.* recovery.img partition.tbl installer.cmd -d /mnt
+unzip ${fastboot_zip} system.img -d /mnt
+sync
 umount /mnt
 
