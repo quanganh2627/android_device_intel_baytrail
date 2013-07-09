6d265479423cb5d4d633ec7e4b535730f7825a65
Hu Gang <gang.a.hu@intel.com>
2013-07-09 14:45:15 +0800

    byt_ffrd10: Add Social Gallery
    BZ: 121256
    
    No social gallery in BYT_T_FFRD10.
    This patch add the social gallery for BYT_T_FFRD10.
    
    Change-Id: Ib3aced34c9da6ccf415427e97c4b8290f028e938
    Signed-off-by: Hu Gang <gang.a.hu@intel.com>
    Reviewed-on: http://android.intel.com:8080/117612
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- byt_t_ffrd10/device.mk
+++ byt_t_ffrd10/device.mk
@@ -219,7 +219,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera app
 PRODUCT_PACKAGES += \
-    IntelCamera
+    IntelCamera \
+    SocialGallery
 
 # Test Camera is for Test only
 ifeq ($(TARGET_BUILD_VARIANT),eng)
