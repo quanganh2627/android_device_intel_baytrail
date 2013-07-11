751fc1d9d2c97fb2b4c2b3652ca96ab847e4dfbb
Ranjan Dutta <ranjan.dutta@intel.com>
2013-07-11 13:40:58 +0530

    Fixed the path for  ifwi on ffrd10 W27
    Change-Id: I54888aba08c66d985af709a0a058a2404da4b3ff
    Signed-off-by: Ranjan Dutta <ranjan.dutta@intel.com>

--- byt_t_ffrd10/BoardConfig.mk
+++ byt_t_ffrd10/BoardConfig.mk
@@ -113,7 +113,11 @@ OVERRIDE_RS_DRIVER := libRSDriver_intel7.so
 BOARD_HAS_ULPMC := true
 
 # Capsule FW PATH
-CAPSULE_BINARY := vendor/intel/fw/PRIVATE/ifwi/baylake/byt_t/capsule.bin
+#CAPSULE_BINARY := vendor/intel/fw/PRIVATE/ifwi/baylake/byt_t/capsule.bin
+CAPSULE_BINARY := prebuilts/intel/vendor/intel/prebuilts/fw/ifwi/baylake/byt_t/capsule.bin
+
 # ULPMC FW PATH
-ULPMC_BINARY := vendor/intel/fw/PRIVATE/ulpmc/ULPMC_FW_PR1.bin
+#ULPMC_BINARY := vendor/intel/fw/PRIVATE/ulpmc/ULPMC_FW_PR1.bin
+ULPMC_BINARY := prebuilts/intel/vendor/intel/prebuilts/fw/ulpmc/ULPMC_V_15_PR1.bin
+
 BOARD_KERNEL_DROIDBOOT_EXTRA_CMDLINE +=  droidboot.use_installer=1 droidboot.installer_usb=/dev/block/sda1 droidboot.installer_file=installer.cmd
