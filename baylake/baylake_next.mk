e944c525a8f4cf66f86d6df6cda6c7ec3a95cb46
Laurent FERT <laurent.fert@intel.com>
2013-07-17 16:18:15 +0200

    NEXT: use specific init.platform.usb.rc
    BZ: 123981
    
    Replace init.platform.usb.rc with specific version for target
    using kernel_next/. The generic version for the platform will be
    ignored.
    This patch copies the original usb.rc to _next version.
    
    Change-Id: I257f3def012500253202b6f5c226f2af7494e7e4
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: http://android.intel.com:8080/119276
    Reviewed-by: Wood, Brian J <brian.j.wood@intel.com>
    Tested-by: Ranostay, Matt <matt.ranostay@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- baylake/baylake_next.mk
+++ baylake/baylake_next.mk
@@ -4,6 +4,11 @@ REF_PRODUCT_NAME := baylake
 KERNEL_SRC_DIR := linux/kernel-next
 
 DROIDBOOT_NO_GUI := true
+include $(LOCAL_PATH)/baylakepath.mk
+
+# Replace platform USB with "next" version - first found has precedence
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/init.byt_next.usb.rc:root/init.platform.usb.rc
 
 # Include base product makefile
 include $(LOCAL_PATH)/baylake.mk
