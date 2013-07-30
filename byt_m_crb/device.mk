397ce77de0777e9339370e5b9a94ea4f52431347
lwan89x <liangx.wang@intel.com>
2013-07-30 19:30:26 +0800

    BYT-M-CRB: Change DPI configuration.
    BZ: 125270
    
    Fix the DPI configuration to pass the CTS android.holo test.
    
    Change-Id: Ia2bd34b872834eec2c6886b6cee893518f7344ab
    Signed-off-by: lwan89x <liangx.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/121897
    Tested-by: Shentu, JiazhenX <jiazhenx.shentu@intel.com>
    Reviewed-by: Zhang, Hongyu <hongyu.zhang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- byt_m_crb/device.mk
+++ byt_m_crb/device.mk
@@ -122,7 +122,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
-    ro.sf.lcd_density=240
+    ro.sf.lcd_density=213
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -259,8 +259,8 @@ PRODUCT_COPY_FILES += \
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_CONFIG := xlarge large tvdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xlarge tvdpi
 
 # usb accessory
 PRODUCT_PACKAGES += \
