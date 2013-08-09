ced0166cb0deb8151bf7bd1762d3038200d2bb8a
Vincent Tinelli <vincent.tinelli@intel.com>
2013-08-09 13:59:46 +0200

    serialno: md5 hash sata serial
    BZ: 129570
    
    hash serial to avoid dupes
    
    Change-Id: I135892710dfd2417179ec536e8f68d8710b3459d
    Signed-off-by: Vincent Tinelli <vincent.tinelli@intel.com>

--- serialno/Android.mk
+++ serialno/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := serialno
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
 LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := bionic/libc/bionic
 LOCAL_STATIC_LIBRARIES := libc libcutils liblog
 include $(BUILD_EXECUTABLE)
 
