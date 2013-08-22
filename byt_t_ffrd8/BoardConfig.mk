80997f7f9cb0f9b1595623a7351fd3892eea1695
Benoit Fradin <benoit.fradin@intel.com>
2013-08-22 11:21:17 +0200

    Modem binaries are not published for FFRD8
    BZ: 131279
    
    Remove extra BOARD_HAVE_MODEM set to false
    
    Change-Id: Iea85cfb32282e8c1f1718ea8f46d3fde32aba9a6
    Signed-off-by: Benoit Fradin <benoit.fradin@intel.com>
    Reviewed-on: http://android.intel.com:8080/127074
    Reviewed-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- byt_t_ffrd8/BoardConfig.mk
+++ byt_t_ffrd8/BoardConfig.mk
@@ -50,7 +50,6 @@ else
 # Disable WIFI for kernel_next bringup
 BOARD_HAVE_WIFI := false
 endif
-BOARD_HAVE_MODEM := false
 INTEL_WIDI := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)
