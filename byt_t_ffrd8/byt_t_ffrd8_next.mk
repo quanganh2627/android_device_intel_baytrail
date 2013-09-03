1c30004137c00a61c2dbc0575db30a30fc0194e4
Wu, Hao <hao.wu@intel.com>
2013-09-03 23:14:59 +0800

    byt_t_ffrd8_next: use specific usb init rc for next branch
    BZ: 133890
    
    For K3.10, it switched to ffs instead of old adb gadget.
    
    Change-Id: I3755c608470835ffbfe94c6ada00a14a3bd38535
    Signed-off-by: Wu, Hao <hao.wu@intel.com>
    Reviewed-on: http://android.intel.com:8080/129840
    Reviewed-by: Wang, Yu Y <yu.y.wang@intel.com>
    Reviewed-by: Wood, Brian J <brian.j.wood@intel.com>
    Reviewed-by: Tang, Richard <richard.tang@intel.com>
    Tested-by: Wang, Yu Y <yu.y.wang@intel.com>
    Reviewed-by: Gross, Mark <mark.gross@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- byt_t_ffrd8/byt_t_ffrd8_next.mk
+++ byt_t_ffrd8/byt_t_ffrd8_next.mk
@@ -2,5 +2,10 @@ PRODUCT_NAME := byt_t_ffrd8_next
 REF_PRODUCT_NAME := byt_t_ffrd8
 
 KERNEL_SRC_DIR := linux/kernel-next
+include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
+
+# Replace platform USB with "next" version - first found has precedence
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/init.byt_next.usb.rc:root/init.platform.usb.rc
 
 include $(LOCAL_PATH)/byt_t_ffrd8.mk
