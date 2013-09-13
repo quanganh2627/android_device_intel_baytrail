c01d3ed203c17aaafec44658ce8bb80a50b872bd
Ranjan Dutta <ranjan.dutta@intel.com>
2013-09-14 05:07:09 +0530

    Setting USE_INTEL_LVSE to false - W37
    Change-Id: Ibc0e5a3966713108623054314103389a72d072b5
    Signed-off-by: Ranjan Dutta <ranjan.dutta@intel.com>

--- baylake/BoardConfig.mk
+++ baylake/BoardConfig.mk
@@ -72,7 +72,7 @@ USE_CSS_2_0 := true
 # Enabled HW accelerated JPEG encoder using VA API
 USE_INTEL_JPEG := false
 # Enabled NXP Premium Audio Effect Libraries
-USE_INTEL_LVSE := true
+USE_INTEL_LVSE := false
 JPEGDEC_USES_GEN := true
 
 ifeq ($(BOARD_KERNEL_CMDLINE),)
