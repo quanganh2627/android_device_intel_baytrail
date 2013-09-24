95866f07bcbd717a3df6eafc0b14d5c75baec1ea
Sergio Aguirre <sergio.a.aguirre.rodriguez@intel.com>
2013-09-24 07:21:06 -0700

    baylake_edk2: disable useless cmdline options
    BZ: 145917
    
    Change-Id: I8423d7b065b24d5522602856c0466612d3ad44bb
    Signed-off-by: Sergio Aguirre <sergio.a.aguirre.rodriguez@intel.com>
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: http://android.intel.com:8080/134230
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- baylake/baylake_edk2.mk
+++ baylake/baylake_edk2.mk
@@ -9,6 +9,12 @@ FORCE_FLASHFILE_NO_OTA := true
 TARGET_PARTITIONING_SCHEME := "full-gpt"
 TARGET_BIOS_TYPE := "uefi"
 
+# Disable SPID param in cmdline
+USE_SPID := false
+
+# Disable serialno param in cmdline
+USE_BL_SERIALNO := true
+
 # Include base product makefile
 include $(LOCAL_PATH)/baylake.mk
 
