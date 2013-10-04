657774e1d2a2a90e73c7a05a2558e6709b22611e
Dale Stimson <dale.b.stimson@intel.com>
2013-10-03 19:32:43 -0700

    Revert "[K3.10] REVERTME - gfx - temp k3.10 ffrd8 - setprop intel.hwc.overlayrgb.enable 0"
    BZ: 142896
    
    This reverts commit 67f5491d2d6ae24ca90a2bfa04ab0f50d8a5689b.
    
    The previous commit was specific to target byt_t_ffrd8_next
    which is no longer valid.  That workaround is replaced for
    all Baytrail targets by the new workaround with change id
    I2ac9338e467782d02a4c90df5706f7f71774cf51.
    
    Change-Id: I0874167d563d5cd086b642eba26aba42b2a35e16
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- byt_t_ffrd8/byt_t_ffrd8_next.mk
+++ byt_t_ffrd8/byt_t_ffrd8_next.mk
@@ -6,8 +6,4 @@ BOARD_HAVE_KNEXT := true
 
 include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
 
-# File init.byt_t_ffrd8_next.gengfx.rc is being added before the other use
-# of this destination, so it will have precedence.
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.byt_t_ffrd8_next.gengfx.rc:root/init.platform.gengfx.rc
-
 include $(LOCAL_PATH)/byt_t_ffrd8.mk
