db60cfd27506605d51a642dee1c510b02cfeafca
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-10-04 11:04:39 +0200

    Fix usb init after switch to kernel 3.10
    BZ: 142948
    
    Change-Id: I5bdaa841deeeda120eb17d23e1fb1d63d6bc8635
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: http://android.intel.com:8080/136246
    Reviewed-by: Heckmann, Nicolas <nicolas.heckmann@intel.com>
    Tested-by: Heckmann, Nicolas <nicolas.heckmann@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- byt_t_ffrd8/byt_t_ffrd8_next.mk
+++ byt_t_ffrd8/byt_t_ffrd8_next.mk
@@ -6,10 +6,6 @@ BOARD_HAVE_KNEXT := true
 
 include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
 
-# Replace platform USB with "next" version - first found has precedence
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/init.byt_next.usb.rc:root/init.platform.usb.rc
-
 # File init.byt_t_ffrd8_next.gengfx.rc is being added before the other use
 # of this destination, so it will have precedence.
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.byt_t_ffrd8_next.gengfx.rc:root/init.platform.gengfx.rc
