e1b8c9d2c7d44ee432b3f90f6c54994bbf5217b8
buildslave <buildslave@buildbot.tl.intel.com>
2013-10-11 16:27:17 +0000

    Automatically revert commits from previous weekly release
    Change-Id: I98763dc697df60d9f9d6f19ac756b39588c4ec4c
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- byt_m_crb/BoardConfig.mk
+++ byt_m_crb/BoardConfig.mk
@@ -61,6 +61,7 @@ BOARD_USES_GENERIC_AUDIO := false
 #GEN is one graphic and video engine
 # Baytrail uses the GEN for the graphic and video
 BOARD_GRAPHIC_IS_GEN := true
+INTEL_FEATURE_ETHERNET := true
 
 # Camera
 # Set USE_CAMERA_STUB to 'true' for Fake Camera builds,
