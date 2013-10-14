30914d8ab086424360907aa94b8ef5e2d5550759
Leo Yan <leo.yan@intel.com>
2013-10-14 09:07:24 +0800

    sensors: Add proximity sensor to device feature
    BZ: 140124
    
    Add proximity sensor to device feature since there is proximity sensor
    on BYT-FFRD8.
    
    Change-Id: Iad702d2a0deb1d1d6fc0ce59b44c067fbefb8ce0
    Signed-off-by: Leo Yan <leo.yan@intel.com>
    Reviewed-on: http://android.intel.com:8080/138192
    Reviewed-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: Chen, Jason CJ <jason.cj.chen@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- byt_t_ffrd8/device.mk
+++ byt_t_ffrd8/device.mk
@@ -354,6 +354,7 @@ PRODUCT_COPY_FILES += \
     $(FRAMEWORK_ETC_PATH)/android.hardware.sensor.compass.xml:$(PERMISSIONS_PATH)/android.hardware.sensor.compass.xml \
     $(FRAMEWORK_ETC_PATH)/android.hardware.sensor.gyroscope.xml:$(PERMISSIONS_PATH)/android.hardware.sensor.gyroscope.xml \
     $(FRAMEWORK_ETC_PATH)/android.hardware.sensor.light.xml:$(PERMISSIONS_PATH)/android.hardware.sensor.light.xml \
+    $(FRAMEWORK_ETC_PATH)/android.hardware.sensor.proximity.xml:$(PERMISSIONS_PATH)/android.hardware.sensor.proximity.xml \
     $(FRAMEWORK_ETC_PATH)/android.hardware.wifi.xml:$(PERMISSIONS_PATH)/android.hardware.wifi.xml \
     $(FRAMEWORK_ETC_PATH)/android.hardware.usb.host.xml:$(PERMISSIONS_PATH)/android.hardware.usb.host.xml \
     $(FRAMEWORK_ETC_PATH)/android.hardware.usb.accessory.xml:$(PERMISSIONS_PATH)/android.hardware.usb.accessory.xml \
