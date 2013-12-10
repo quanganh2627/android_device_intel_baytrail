83c385cf2927802699f55d072fe807e8be6df5d7
Benoit Fradin <benoit.fradin@intel.com>
2013-12-10 17:02:14 +0100

    Remove usage of libsparse and usage of tmp file : directly flash sparse buffers
    BZ: 157254
    
    Remove droidboot scratch size from BoardConfig
    
    Change-Id: I86812bdac4850ded672af24c59b02a71f34713ba
    Signed-off-by: Benoit Fradin <benoit.fradin@intel.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,9 +46,6 @@ TARGET_DROIDBOOT_USB_MODE_FASTBOOT := true
 TARGET_MAKE_NO_DEFAULT_OTA_PACKAGE := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(HOST_OUT)/bin/releasetools.py
 OTA_FROM_TARGET_FILES := $(HOST_OUT)/bin/ota_from_target_files
-# Size in megabytes of Droidboot USB buffer, must be as large
-# as the largest image we need to flash
-DROIDBOOT_SCRATCH_SIZE := 100
 DROIDBOOT_USE_INSTALLER := true
 endif
 
