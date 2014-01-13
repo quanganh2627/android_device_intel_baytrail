e4e228c3cce60ccc38209afe2a4f909a8a03decf
Markus Grape <markus.grape@intel.com>
2014-01-13 11:27:59 +0100

    Add thermal-conf.xml and mixin for thermal-daemon
    Added per platform tunable thermal-conf.xml to baytrail.
    Refer to man pages under thermal-daemon for format.
    
    Also added thermal-daemon as a mixin for thermals.
    
    For: APDEV-757
    Change-Id: I1dd06fbde1ad8a2694c217d73076d738f55760cf
    Depends-Change-Id: I0a72da1b22a6e0724a1471d88b16f241264da871
    Depends-Change-Id: I74b5240c0562e36a3864980e26ecc0aa6fdcfb28
    Signed-off-by: Markus Grape <markus.grape@intel.com>

--- baytrail.mk
+++ baytrail.mk
@@ -7,8 +7,9 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.baytrail.rc:root/init.baytrail.rc \
 	$(LOCAL_PATH)/init.recovery.baytrail.rc:root/init.recovery.baytrail.rc \
 	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml\
-	$(LOCAL_PATH)/camera_profiles.xml:system/etc/camera_profiles.xml
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/camera_profiles.xml:system/etc/camera_profiles.xml \
+	$(LOCAL_PATH)/thermal-conf.xml:system/etc/thermal-daemon/thermal-conf.xml
 
 $(call inherit-mixin, cpu-arch, slm)
 
