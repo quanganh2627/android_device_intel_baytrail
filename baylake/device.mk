b60ad4d26f40d12709f978591acbfbbcf7ec2060
Ryan Saffores <ryan.d.saffores@intel.com>
2014-01-17 10:08:36 -0800

    Adding targets for L3 WV Modular DRM for Baylake
    BZ: 163118
    
    Adding library and apk for L3 WV Modular DRM for Baylake
    
    Change-Id: I02e8bd9c336a56f8a4cdd1ad8525264608e1b7fa
    Signed-off-by: Ryan Saffores <ryan.d.saffores@intel.com>

--- baylake/device.mk
+++ baylake/device.mk
@@ -333,10 +333,12 @@ PRODUCT_PACKAGES += com.google.widevine.software.drm.xml \
     libWVStreamControlAPI_L1 \
     libwvdrm_L1
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
- PRODUCT_PACKAGES += \
-     WidevineSamplePlayer
-endif
+PRODUCT_PACKAGES_ENG += WidevineSamplePlayer
+
+# WV Modular
+PRODUCT_PACKAGES += libwvdrmengine
+
+PRODUCT_PACKAGES_ENG += ExoPlayerDemo
 
 # Intel VPP/FRC
 PRODUCT_PACKAGES += \
