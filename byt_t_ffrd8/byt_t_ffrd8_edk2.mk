26334e3522538ca46a87033dc9087c10e563cdc8
Richard Chevalier <richard.chevalier@intel.com>
2014-01-20 14:59:41 +0100

    Switch ffrd8_next target to uefi
    BZ: 165889
    
    Change-Id: Ibc44a34bb5c732ca5167e3480d330f42770546c1
    Signed-off-by: Richard Chevalier <richard.chevalier@intel.com>

--- byt_t_ffrd8/byt_t_ffrd8_edk2.mk
+++ byt_t_ffrd8/byt_t_ffrd8_edk2.mk
@@ -1,13 +1,7 @@
 PRODUCT_NAME := byt_t_ffrd8_edk2
 REF_PRODUCT_NAME := byt_t_ffrd8
 
-BOARD_HAS_CAPSULE := true
-FORCE_FLASHFILE_NO_OTA := true
-TARGET_OS_SIGNING_METHOD := isu
-TARGET_PARTITIONING_SCHEME := "full-gpt"
-TARGET_BIOS_TYPE := "uefi"
-HAS_SPINOR := true
-USE_FPT := true
+include $(LOCAL_PATH)/has_uefi.mk
 
 include $(LOCAL_PATH)/byt_t_ffrd8.mk
 
--- byt_t_ffrd8/byt_t_ffrd8_next.mk
+++ byt_t_ffrd8/byt_t_ffrd8_next.mk
@@ -4,6 +4,8 @@ REF_PRODUCT_NAME := byt_t_ffrd8
 KERNEL_SRC_DIR := linux/kernel-next
 BOARD_HAVE_KNEXT := true
 
+include $(LOCAL_PATH)/has_uefi.mk
+
 include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
 
 include $(LOCAL_PATH)/byt_t_ffrd8.mk
