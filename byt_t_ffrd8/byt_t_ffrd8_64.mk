daa1a889b497f0fd53e2219718a7b1288a992bc2
Olivier Fourdan <olivier.fourdan@intel.com>
2014-01-21 10:30:53 +0100

    Switch 64bit FFRD8 to EDK2
    BZ: 165910
    
    Use EDK2 UEFI for FFRD8 64bit variant.
    
    Change-Id: I240cf3e3adb9683cef50c3a8600ea25bf0cd254c
    Signed-off-by: Olivier Fourdan <olivier.fourdan@intel.com>

--- byt_t_ffrd8/byt_t_ffrd8_64.mk
+++ byt_t_ffrd8/byt_t_ffrd8_64.mk
@@ -3,6 +3,8 @@ REF_PRODUCT_NAME := byt_t_ffrd8
 
 BOARD_USE_64BIT_KERNEL := true
 
+include $(LOCAL_PATH)/has_uefi.mk
+
 include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
 
 include $(LOCAL_PATH)/byt_t_ffrd8.mk
