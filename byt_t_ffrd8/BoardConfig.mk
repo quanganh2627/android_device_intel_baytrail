378ba3932ace87f92dad81b3f4414bb530257b25
Gary Smith <gary.k.smith@intel.com>
2014-02-10 15:21:07 +0000

    [PORT FROM MAIN]Enable SurfaceFlinger to request a final CSC from the HWC in all cases.
    BZ: 156459
    
    For Google Miracast acceleration, The h264 encoder wants the source frames
    to be delievered in NV12 format. This means that the HWC has to perform a
    final CSC on any SF compositied frames as OGL cannot composite into NV12.
    
    This compile time flag enables this behaviour in both the HWC and the
    SurfaceFlinger. This patch is also needed to fix a CTS bug.
    
    Change-Id: I5d58c4dec404db45b930bb1794d04106a4d6589d
    Category: device enablement
    Domain: Graphics-SF
    Origin: internal
    Upstream-Candidate: No
    Signed-off-by: Gary Smith <gary.k.smith@intel.com>

--- byt_t_ffrd8/BoardConfig.mk
+++ byt_t_ffrd8/BoardConfig.mk
@@ -120,6 +120,9 @@ BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.vblankoffdelay=1 \
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 
+# Allow HWC to perform a final CSC on virtual displays
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
 # Graphics - MIPI
 # List of panel ids supported:
 #
