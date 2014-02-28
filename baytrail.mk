4cd85dc85329099f89b7b46a33138836641b5708
Jacques Imougar <jacquesx.imougar@intel.com>
2014-02-28 11:26:14 +0100

    IPTrak: Add module in byt platform
    BZ: 170311
    
    Change-Id: Id576e6c1a04602f4f7aee536ce70664666a02bb3
    Signed-off-by: Jacques Imougar <jacquesx.imougar@intel.com>
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>

--- baytrail.mk
+++ baytrail.mk
@@ -111,6 +111,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rfid_monzaxd
 
+# Add IPTrak
+PRODUCT_PACKAGES_DEBUG += \
+    IPTrak
+
 # Identity Protection Technology (IPT)
 PRODUCT_PACKAGES += \
     libiha \
