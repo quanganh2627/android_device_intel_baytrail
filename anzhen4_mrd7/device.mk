3092f7068d6886bf3ef4303ce487944144012622
Peter,Zhang <peter.zhang@intel.com>
2014-03-19 09:14:09 +0800

    Enabling BCM4752 GPS on MRD7
    BZ:65
    
    Change-Id: I91810710b4c899595f5990621081e52711d3c812
    Borig-Change-ID: Ibd818c0715683089dd07dc94877d37cddd79ad0f
    Signed-off-by: Peter,Zhang <peter.zhang@intel.com>
    Reviewed-on: http://192.168.1.79/gerrit/5498
    Reviewed-by: b561 <b561@borqs.com>
    Reviewed-on: http://192.168.1.79/gerrit/6469

--- anzhen4_mrd7/device.mk
+++ anzhen4_mrd7/device.mk
@@ -205,6 +205,13 @@ endif
     libdx-crys \
     start-sep
 
+# BCM4752 GPS
+PRODUCT_PACKAGES += \
+    gps_bcm_4752
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.spid.gps.pmm=disabled \
+    ro.spid.gps.tty=ttyMFD1
+
 # bluetooth
 PRODUCT_PACKAGES += \
     bt_rtl8723b
