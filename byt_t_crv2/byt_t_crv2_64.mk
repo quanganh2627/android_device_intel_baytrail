f205f3c09772ebfa97da39bd354ca29ad2512d98
Benoit Fradin <benoit.fradin@intel.com>
2014-03-20 10:48:12 +0100

    [x64] [REVERTME] Force OS loader policy to fake for BYT 64 bits
    BZ: 180567
    
    Change-Id: I7ca7a16a043a23df6790508e96b423bcc62a92e0
    Signed-off-by: Benoit Fradin <benoit.fradin@intel.com>

--- byt_t_crv2/byt_t_crv2_64.mk
+++ byt_t_crv2/byt_t_crv2_64.mk
@@ -3,6 +3,9 @@ REF_PRODUCT_NAME := byt_t_crv2
 
 BOARD_USE_64BIT_KERNEL := true
 
+# Revert-me force OS loader polciy to fake until GetBatteryStatus
+OSLOADER_EM_POLICY = fake
+
 include $(LOCAL_PATH)/byt_t_crv2_path.mk
 
 include $(LOCAL_PATH)/byt_t_crv2.mk
--- byt_t_ffrd8/byt_t_ffrd8_64.mk
+++ byt_t_ffrd8/byt_t_ffrd8_64.mk
@@ -3,6 +3,9 @@ REF_PRODUCT_NAME := byt_t_ffrd8
 
 BOARD_USE_64BIT_KERNEL := true
 
+# Revert-me force OS loader polciy to fake until GetBatteryStatus
+OSLOADER_EM_POLICY = fake
+
 include $(LOCAL_PATH)/has_uefi.mk
 
 include $(LOCAL_PATH)/byt_t_ffrd8_path.mk
