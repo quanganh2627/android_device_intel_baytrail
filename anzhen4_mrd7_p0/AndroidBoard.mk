c7598b3af5002ffd3ee7ad1401d221498dcd37d0
Peter,Zhang <peter.zhang@intel.com>
2014-04-10 13:56:33 +0800

    Add P0 build target
    BZ:89
    
    mrd7 is for p0.5, this temporarily for p0
    
    Change-Id: I8a92c08d45ca921b097c3da54204127b6e2fe787
    Borig-Change-ID: I7db47a634870884ae14701ac37327733ec6927ce
    Signed-off-by: Peter,Zhang <peter.zhang@intel.com>
    Reviewed-on: http://192.168.1.79/gerrit/5521
    Reviewed-by: b561 <b561@borqs.com>
    Reviewed-on: http://192.168.1.79/gerrit/6517

---
+++ anzhen4_mrd7_p0/AndroidBoard.mk
@@ -0,0 +1,8 @@
+include $(PLATFORM_PATH)/AndroidBoard.mk
+
+# parameter-framework
+include $(DEVICE_CONF_PATH)/parameter-framework/AndroidBoard.mk
+
+# FG config file
+include $(DEVICE_CONF_PATH)/fg_config/AndroidBoard.mk
+
---
+++ anzhen4_mrd7_p0/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/anzhen4_mrd7_p0.mk
