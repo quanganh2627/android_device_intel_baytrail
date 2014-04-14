16aa34ec0586dd7d962f023a9d0b2669ab996504
Olivier Fourdan <olivier.fourdan@intel.com>
2014-04-14 10:17:25 +0200

    Create target for BYT-T FFD8 EDK2 x64
    BZ: 186518
    
    Create new target "byt_t_ffrd8_edk2_64" for BYT-T FFD8 EDK2 x64
    by renaming the existing "byt_t_ffrd8_64" which was already using
    EDK2 UEFI BIOS.
    
    Change-Id: I371cb992ee04a65ac7373454d3e9376e0c135d29
    Signed-off-by: Olivier Fourdan <olivier.fourdan@intel.com>

--- byt_t_ffrd8/AndroidProducts.mk
+++ byt_t_ffrd8/AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/byt_t_ffrd8.mk \
-    $(LOCAL_DIR)/byt_t_ffrd8_64.mk \
     $(LOCAL_DIR)/byt_t_ffrd8_next.mk \
-    $(LOCAL_DIR)/byt_t_ffrd8_edk2.mk
+    $(LOCAL_DIR)/byt_t_ffrd8_edk2.mk \
+    $(LOCAL_DIR)/byt_t_ffrd8_edk2_64.mk
+
