54536fdb7adb5f9169702eba4f6060e41d3ada39
Tu, Jin <jin.tu@intel.com>
2014-05-20 09:36:17 +0800

    Enable 3G Dongle for anzhen4 ww17-ext
    Change-Id: If76afbcfdb82c896c6dd05c310e0e75bdb798b12
    Borig-Change-ID: Ibc0a0ec93add6c6ff50a45bb032664e3c07407cb
    Signed-off-by: Tu, Jin <jin.tu@intel.com>
    Reviewed-on: http://192.168.1.79/gerrit/6067
    Reviewed-by: b549 <b549@borqs.com>
    Reviewed-on: http://192.168.1.79/gerrit/6759

--- anzhen4_mrd7_w/anzhen4_mrd7_w_64.mk
+++ anzhen4_mrd7_w/anzhen4_mrd7_w_64.mk
@@ -1,6 +1,22 @@
 PRODUCT_NAME := anzhen4_mrd7_w_64
 # REF_PRODUCT_NAME := byt_t_crv2
 
+#3G dongle support.if true,only support 3g dongle,no support modem; otherwise, only support modem,no support 3g dongle
+SUPPORT_3G_DONGLE_ONLY := true
+
+ifeq ($(SUPPORT_3G_DONGLE_ONLY),true)
+# Dongle files
+PRODUCT_COPY_FILES += \
+		$(LOCAL_PATH)/dongle/connect-chat:system/etc/ppp/connect-chat \
+		$(LOCAL_PATH)/dongle/ip-up:system/etc/ppp/ip-up \
+	        $(LOCAL_PATH)/dongle/ip-down:system/etc/ppp/ip-down \
+	        $(LOCAL_PATH)/dongle/chat:system/xbin/chat \
+	        $(LOCAL_PATH)/dongle/usb_modeswitch:system/xbin/usb_modeswitch
+#Add 3G Dongle
+PRODUCT_PACKAGES += \
+   Dongle
+endif
+
 BOARD_USE_64BIT_KERNEL := true
 
 include $(LOCAL_PATH)/anzhen4_mrd7_w_path.mk
