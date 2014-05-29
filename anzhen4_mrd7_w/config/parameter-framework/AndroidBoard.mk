922f210f68bce29553fea4f77871f69d238ef907
b532 <b532@borqs.com>
2014-05-29 17:46:35 +0800

    add new target anzhen4_mrd7_w.
    borqsbtx: 0000
    add new target anzhen4_mrd7_w.
    
    Change-Id: I11211aa26b48deefa7f49959ec086bfa6ef6626e
    Borig-Change-ID: I31c75aa659438d346c9be689db02f6fd8e0a3287
    Reviewed-on: http://192.168.1.79/gerrit/6405
    Reviewed-by: Candle Sun

---
+++ anzhen4_mrd7_w/config/parameter-framework/AndroidBoard.mk
@@ -0,0 +1,133 @@
+include $(PLATFORM_CONF_PATH)/parameter-framework/AndroidBoard.mk
+
+LOCAL_PATH := $(DEVICE_CONF_PATH)/parameter-framework
+
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := parameter-framework.audio.anzhen4_mrd7_w
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    parameter-framework.audio.anzhen4_mrd7_w.nodomains \
+    AudioConfigurableDomains.xml
+include $(BUILD_PHONY_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := parameter-framework.audio.anzhen4_mrd7_w.nodomains
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    parameter-framework.audio.baytrail \
+    SysfsPmdownTimeBytcrSubsystem.xml \
+    TI_TLV320AIC3100Subsystem.xml \
+    AudioClass.xml
+
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+LOCAL_REQUIRED_MODULES += ParameterFrameworkConfiguration.xml
+else
+LOCAL_REQUIRED_MODULES += ParameterFrameworkConfigurationNoTuning.xml
+endif
+
+include $(BUILD_PHONY_PACKAGE)
+
+
+##################################################
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := AudioClass.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Audio
+LOCAL_SRC_FILES := XML/Structure/Audio/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TI_TLV320AIC3100Subsystem.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Audio
+LOCAL_SRC_FILES := XML/Structure/Audio/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SysfsPmdownTimeBytcrSubsystem.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Structure/Audio
+LOCAL_SRC_FILES := XML/Structure/Audio/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+ifeq ($(BOARD_USES_AUDIO_HAL_CONFIGURABLE),true)
+
+## Audio Tuning + Routing
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := AudioConfigurableDomains.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Settings/Audio
+
+LOCAL_REQUIRED_MODULES := \
+        hostDomainGenerator.sh \
+        parameter-framework.audio.anzhen4_mrd7_w.nodomains
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): MY_SRC_FILES := \
+        $(TARGET_OUT_ETC)/parameter-framework/ParameterFrameworkConfiguration.xml \
+        $(LOCAL_PATH)/criteria.txt \
+        $(LOCAL_PATH)/XML/Settings/Audio/AudioConfigurableDomains.xml \
+        $(LOCAL_PATH)/XML/Settings/Audio/anzhen4_mrd7_w_routing_tlv320aic3100.pfw
+
+$(LOCAL_BUILT_MODULE): $(LOCAL_REQUIRED_MODULES)
+	$(hide) mkdir -p $(dir $@)
+	bash hostDomainGenerator.sh $(MY_SRC_FILES) > $@
+
+else
+
+## Audio Tuning only
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := AudioConfigurableDomains.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework/Settings/Audio
+LOCAL_SRC_FILES := XML/Settings/Audio/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+endif
+
+##################################################
+
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+
+### Engineering build: Tuning allowed
+
+# baylake
+include $(CLEAR_VARS)
+LOCAL_MODULE := ParameterFrameworkConfiguration.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework
+LOCAL_SRC_FILES := XML/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+else
+
+### Userdebug build: NoTuning allowed
+
+
+# baytrail
+include $(CLEAR_VARS)
+LOCAL_MODULE := ParameterFrameworkConfigurationNoTuning.xml
+LOCAL_MODULE_STEM := ParameterFrameworkConfiguration.xml
+LOCAL_SRC_FILES := XML/ParameterFrameworkConfigurationNoTuning.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/parameter-framework
+include $(BUILD_PREBUILT)
+
+endif
+
