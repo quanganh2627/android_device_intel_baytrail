6e7fd311a838218a2e628faaef6523cd340746cf
lyan11 <mario.lu@intel.com>
2014-06-11 11:19:11 +0800

    result of rebase lose the configuration of dongle in folder /device/intel/baytrail/catalog_dev, re-add the configuration of dongle
    BZ: 511
    
    add configuration of dongle setting and fix the rebase issues for dongle APP
    
    Change-Id: If698394fea6c1616edac91802772dd7183f6cc7c

--- catalog_dev/catalog_dev_64.mk
+++ catalog_dev/catalog_dev_64.mk
@@ -5,4 +5,16 @@ BOARD_USE_64BIT_KERNEL := true
 
 include $(LOCAL_PATH)/catalog_dev_path.mk
 
+# Dongle files
+PRODUCT_COPY_FILES += \
+		$(LOCAL_PATH)/dongle/connect-chat:system/etc/ppp/connect-chat \
+		$(LOCAL_PATH)/dongle/ip-up:system/etc/ppp/ip-up \
+		$(LOCAL_PATH)/dongle/ip-down:system/etc/ppp/ip-down \
+		$(LOCAL_PATH)/dongle/chat:system/xbin/chat \
+		$(LOCAL_PATH)/dongle/usb_modeswitch:system/xbin/usb_modeswitch
+
+#Add 3G Dongle
+PRODUCT_PACKAGES += \
+   Dongle
+
 include $(LOCAL_PATH)/catalog_dev.mk
