6a130913517eecd7b6d571aa8c33ef8ca0d65889
hding3 <haitao.ding@intel.com>
2014-06-23 17:13:20 +0800

    HEVC decoder delivery as a part of libmfxsw32
    BZ: 206265
    
    HEVC decoder delivery as a part of libmfxsw32
    
    Change-Id: I2f244c7cbfda98e3ff0b64905e1600bea43f3e0f
    Signed-off-by: hding3 <haitao.ding@intel.com>

--- byt_t_ffrd8/device.mk
+++ byt_t_ffrd8/device.mk
@@ -67,8 +67,10 @@ PRODUCT_PACKAGES += \
 # Media SDK and OMX IL components
 PRODUCT_PACKAGES += \
     libmfxhw32 \
+    libmfxsw32 \
     libmfx_omx_core \
     libmfx_omx_components_hw \
+    libmfx_omx_components_sw \
     libgabi++-mfx \
     libstlport-mfx
 
