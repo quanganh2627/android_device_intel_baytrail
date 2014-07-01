42964b2d153047a5db062decb23910da059a992a
Benoit Fradin <benoit.fradin@intel.com>
2014-07-01 13:59:48 +0200

    Update installer for BYT-CR
    BZ:197578
    
    Enable SD card installer support for BYT-CR
    
    Change-Id: I112de091975b7d8809765b93a49481d05582d914
    Signed-off-by: yesheng xu <yesheng.xu@intel.com>

--- installer.cmd
+++ installer.cmd
@@ -1,16 +1,18 @@
-oem write_osip_header
-flash:boot#/installer/boot.img
-flash:recovery#/installer/recovery.img
-flash:fastboot#/installer/droidboot.img
+oem wipe ESP
+oem wipe reserved
 oem start_partitioning
 oem partition /installer/partition.tbl
 erase:factory
-erase:cache
 erase:system
+erase:cache
 erase:config
 erase:logs
-erase:misc
 erase:data
 oem stop_partitioning
-flash:system#/installer/system.img.gz
+flash:ESP#/installer/esp.img
+flash:fastboot#/installer/droidboot.img
+flash:boot#/installer/boot.img
+flash:recovery#/installer/recovery.img
+flash:system#/installer/system.img
+flash:capsule#/installer/BYTC__X64_R_2014_24_4_01_SecEnabled.fv
 continue
