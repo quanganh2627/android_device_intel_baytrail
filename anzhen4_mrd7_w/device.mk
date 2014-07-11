ba97adc60604a5bbc22d1dac20d23adae03d7737
Peter,Zhang <peter.zhang@intel.com>
2014-07-11 09:46:58 +0800

    [WW23-WW26]GFX: update ro.opengles.version to report ES 3.1
    BZ: 202025
    
    Update ro.opengles.version to report ES 3.1.
    
    Change-Id: I51515703fac5e544355acc68c74623c5b6000fcf
    Signed-off-by: Peter,Zhang <peter.zhang@intel.com>

--- anzhen4_mrd7_w/device.mk
+++ anzhen4_mrd7_w/device.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
     ufo_test
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196608 \
+    ro.opengles.version=196609 \
     ro.sf.lcd_density=160
 
 # Version of mandatory blankphone
