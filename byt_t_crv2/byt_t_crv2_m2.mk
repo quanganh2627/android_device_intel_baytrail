4c18fce0c0bd6673cd3930b5e35beb020ed0b8d9
Yang, Woody <woody.yang@intel.com>
2014-09-03 13:39:20 +0800

    M.2 GPS driver porting.
    Change-Id: Icafbf21ce909c25859aa64ad3a5e3f035f117e1f
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-13078
    Signed-off-by: Frode Isaksen <frodex.isaksen@intel.com>
    Reviewed-on: https://android.intel.com:443/310096

--- byt_t_crv2/byt_t_crv2_m2.mk
+++ byt_t_crv2/byt_t_crv2_m2.mk
@@ -9,6 +9,11 @@ BOARD_HAVE_MODEM := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/init.byt_t_crv2_m2.rc:root/init.byt_t_crv2.rc
 
+# M.2 GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/m2gps/gps.byt_t_crv2.so:system/lib/hw/gps.byt_t_crv2.so \
+    $(LOCAL_PATH)/config/m2gps/Supl-Config.xml:system/etc/Supl-Config.xml
+
 include $(LOCAL_PATH)/byt_t_crv2_path.mk
 
 include $(LOCAL_PATH)/byt_t_crv2.mk
