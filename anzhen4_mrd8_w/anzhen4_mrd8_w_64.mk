af3a4d39f5c638b4f36406594d778e6d4339fa22
xihua.chen <xihua.chen@intel.com>
2014-09-18 00:26:44 +0800

    mrd:add mrd project configure
    add mrd configure duplicated from crv2
    
    Change-Id: I37ca66c0158758e2a690819a5e9d3cae13b9b29d
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-1488
    Signed-off-by: xihua.chen <xihua.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/264972

---
+++ anzhen4_mrd8_w/anzhen4_mrd8_w_64.mk
@@ -0,0 +1,8 @@
+PRODUCT_NAME := anzhen4_mrd8_w_64
+REF_PRODUCT_NAME := anzhen4_mrd8_w
+
+BOARD_USE_64BIT_KERNEL := true
+
+include $(LOCAL_PATH)/anzhen4_mrd8_w_path.mk
+
+include $(LOCAL_PATH)/anzhen4_mrd8_w.mk
